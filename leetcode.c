789547de843b5b2b249c905e74d0a6798caf330c
gamejoye <gamejoye@gmail.com>
2022-01-17 23:05:17 +0800

    提交了1220的题解

--- leetcode.c
+++ leetcode.c
@@ -16,7 +16,7 @@ for(i=0,j=numsSize-1;i<=j;i++,j--)
 return -1;
 }
 
-//35.搜索插入位置
+//35.搜索插入位置(简单)
 int searchInsert(int* nums, int numsSize, int target){
 int i,j;
 for(i=0,j=numsSize-1;i<=j;i++,j--)
@@ -40,3 +40,18 @@ return i;
 //pratice ovo
 //pratic 1 uou
 //pratice 2 qaq
+//1220.统计原因字序列数目(困难)（动态规划）
+int countVowelPermutation(int n){
+    long long mod = 1e9 + 7;
+long dp[5][20000]={{1},{1},{1},{1},{1}};
+long i;
+for(i=1;i<n;i++)
+{
+dp[0][i]=(dp[1][i-1]+dp[2][i-1]+dp[4][i-1])%mod;
+dp[1][i]=(dp[2][i-1]+dp[0][i-1])%mod;
+dp[2][i]=(dp[1][i-1]+dp[3][i-1])%mod;
+dp[3][i]=(dp[2][i-1])%mod;
+dp[4][i]=(dp[2][i-1]+dp[3][i-1])%mod;
+}
+return (dp[4][n-1]+dp[3][n-1]+dp[2][n-1]+dp[1][n-1]+dp[0][n-1])%mod;
+}
