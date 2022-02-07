88cd568044273d1c3f5ffb82e475c48e4efc849f
gamejoye <gamejoye@gmail.com>
2022-02-07 11:41:14 +0800

    提交了1405的题解

--- greed.c
+++ greed.c
@@ -12,3 +12,42 @@ for(i=0;i<numsSize;i++)
 }
 return false;
 }
+
+//1405.最长快乐字符串(大概理解了吧)(看起来简单，做起来难)
+typedef struct {
+    int freq;
+    char ch;
+} Pair;
+
+int cmp(const void * pa, const void * pb) {
+    return ((Pair *)pb)->freq - ((Pair *)pa)->freq;
+}
+
+char * longestDiverseString(int a, int b, int c){
+    Pair arr[3]={{a,'a'},{b,'b'},{c,'c'}};
+    char * res = (char *) malloc ( (sizeof ( char ) ) * ( a+b+c+1 ) );
+    int post = 0;
+    while(1)
+    {
+        qsort(arr, 3, sizeof(Pair), cmp);
+        bool hasNext = false;
+        for(int i=0;i<3;i++)
+        {
+            int freq = arr[i].freq;
+            int ch = arr[i].ch;
+            if(freq<=0)
+            break;
+            if(post>=2&&res[post-2]==ch&&res[post-1]==ch)
+            continue;
+            hasNext=true;
+            res[post++] = ch;
+            arr[i].freq--;
+            break;
+        }
+        if(hasNext==false)
+        break;
+    }
+    res[post]='\0';
+    return res;
+}
+
